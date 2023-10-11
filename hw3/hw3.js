014abf2768bb899399d4e316217d60cfd3363fee
EkaterinaMikhalko <mikhalkoea@gmail.com>
2023-10-12 01:18:50 +0300

    Added hw3

---
+++ hw3/hw3.js
@@ -0,0 +1,85 @@
+/*let password = 'пароль';
+let text = (prompt ('Введите пароль'));
+
+if (text === password) {
+    alert ('Пароль введен верно');
+} else {
+    alert ('Пароль введен неправильно');
+}
+
+let c = 2;
+if (c>0 && c<10) {
+   alert ('Верно');
+} else {
+    alert ('Неверно');
+}
+
+let d = 101;
+let e = 10;
+
+if (d>100 || e>100) {
+    alert ('Верно');
+} else {
+    alert ('Неверно');
+}
+
+let a = '2';
+let b = '3';
+alert ((Number (a)) + (Number (b)));*/
+
+
+let monthNumber = 12;
+switch (monthNumber) {
+    case 1:
+    console.log('Зима');
+        break;
+
+    case 2:
+    console.log('Зима');
+        break;
+
+    case 3:
+    console.log('Весна');
+        break;
+
+    case 4:
+    console.log('Весна');
+        break;
+
+    case 5:
+    console.log('Весна');
+        break;
+
+    case 6:
+    console.log('Лето');
+        break;
+
+    case 7:
+    console.log('Лето');
+        break;
+
+    case 8:
+    console.log('Лето');
+        break;
+
+    case 9:
+    console.log('Осень');
+        break;
+    
+    case 10:
+    console.log('Осень');
+        break;
+
+    case 11:
+    console.log('Осень');
+        break;
+
+    case 12:
+    console.log('Зима');
+        break;
+
+    default:
+
+    console.log('Неправильное значение')
+        break;
+}
