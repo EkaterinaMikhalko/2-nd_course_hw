a8f472b882b2ac6a4e6c746c7d3ff55055ac2b4c
EkaterinaMikhalko <mikhalkoea@gmail.com>
2023-10-14 15:29:03 +0300

    Changed hw3, mobile.scss

--- scripts/hw3/hw3.js
+++ scripts/hw3/hw3.js
@@ -31,51 +31,27 @@ alert ((Number (a)) + (Number (b)));*/
 let monthNumber = 12;
 switch (monthNumber) {
     case 1:
-    console.log('Зима');
-        break;
-
     case 2:
+    case 12:
     console.log('Зима');
         break;
 
     case 3:
-    console.log('Весна');
-        break;
-
     case 4:
-    console.log('Весна');
-        break;
-
     case 5:
     console.log('Весна');
         break;
 
     case 6:
-    console.log('Лето');
-        break;
-
     case 7:
-    console.log('Лето');
-        break;
-
     case 8:
     console.log('Лето');
         break;
 
     case 9:
-    console.log('Осень');
-        break;
-    
     case 10:
-    console.log('Осень');
-        break;
-
     case 11:
     console.log('Осень');
-        break;
-
-    case 12:
-    console.log('Зима');
         break;
 
     default:
