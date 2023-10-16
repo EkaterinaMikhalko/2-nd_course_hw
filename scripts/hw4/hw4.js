c8f7a4a924dcb9f17262e53352d2735a1ff37aad
EkaterinaMikhalko <mikhalkoea@gmail.com>
2023-10-17 01:15:33 +0300

    Added hw4

---
+++ scripts/hw4/hw4.js
@@ -0,0 +1,49 @@
+/*let i = 0;
+while (i<2){
+    alert ('Привет');
+    i++;
+
+}
+
+let i = 1;
+while (i<=5){
+    console.log (i);
+    i++;
+}
+
+let i = 7;
+while (i<=22){
+    console.log (i);
+    i++;
+}
+
+const worker = {
+    'Коля': '200',
+    'Вася': '300',
+    'Петя': '400',
+}
+
+for (let salary in worker){
+    console.log (`${salary} - зарплата ${worker[salary]} долларов`);
+}
+
+
+let num = 0;
+let n;
+for(n = 1000; n>=50; n=n/2){
+    num=num+1;
+}
+console.log('Количество итераций: ', num);
+console.log('Результат: ', n);
+
+*/
+
+let i;
+
+for(i = 1; i<=31; i=i+7){
+    console.log(`Сегодня пятница, ${i}-е число. Необходимо подготовить отчет.`)
+}
+
+
+
+
